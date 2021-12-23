b979b6590e0aa5ea477447170b5676de88646a04
dnms445 <dnms445@gmail.com>
2021-12-23 16:00:32 +0900

    otp program

--- src/otp.java
+++ src/otp.java
@@ -1,11 +1,30 @@
-public class otp {
-    public static void main(String[] args){
-        int var1 = 10;
-        int var2 = 012;
-        int var3 = 0xA;
+@RequestMapping(value = "/account/otp.do")
 
-        System.out.println(var1);
-        System.out.println(var2);
-        System.out.println(var3);
+public String otp (
+    @ModelAttribute("searchVO") LoginVO searchVO,
+    RedirectAttributes redirectAttributes,
+    HttpServletRequest request,
+    ModelMap model) throws Exception{
+
+    byte[] buffer = new byte[5 + 5 * 5];
+    new Random().nextBytes(buffer);
+    Base32 codec = new Base32();
+    byte[] secretKey = Arrays.copyOf(buffer, 10);
+    byte[] bEncodedKey = codec.encode(secretKey);
+
+    String encodedKey = new String(bEncodedKey);
+
+    String QrUrl = getQRBarcodeURL("admin", "facbank", encodedKey);
+
+    model.addAttribute("encodedKey", encodedKey);
+    model.addAttribute("QrUrl", QrUrl);
+
+    return "tiles:bsite/account/login/otp";
+        }
+    public static String getQRBarcodeURL(String user, String host, String secret){
+    String format = "";
+
+    return String.format(format, user, host, secret);
+
     }
 }
